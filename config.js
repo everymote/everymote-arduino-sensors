4ad205d671a22c3d661ff8478d3d2aafef639ce7
Marcus <marcus.h.olsson@gmail.com>
2013-05-07 00:32:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports.getPosition = function(){};
-module.exports.cosm = {apiKey:'y5ZP-941i1rHffUY1fm_0VBSvoSSAKxjbUV4WFppUUZIbz0g',
+module.exports.cosm = {apiKey:'ddddddd',
 						feed:125420};
 module.exports.serial = '/dev/tty.usbmodem1431';
 //module.exports.getPosition = function(){return {"lat":55.59719,"lon":12.9794}};};
